f552416c8746a48bb8281ac35a198469bda77f29
KHALEED\mulla <mullamohammadk123@gmail.com>
2025-04-04 21:02:17 +0530

    Fixed clock update issue and added time formatting

--- Digital_Clock_Seperator.py
+++ Digital_Clock_Seperator.py
@@ -1,3 +1,13 @@
 import tkinter as tk
 import time
 
+def update_time():
+    h = str(time.strftime("%H"))
+    m = str(time.strftime("%M"))
+    s = str(time.strftime("%S"))
+    
+    # Update label text with full time
+    label_hr.config(text=f"{h}:{m}:{s}")  
+    
+    # Call this function again after 1000ms (1 second)
+    label_hr.after(1000, update_time)
